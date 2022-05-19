2b263fb65fe55f033f4f38d3cdce0f71da7e3589
MohJumper <qureshimemo@gmail.com>
2022-05-18 20:52:24 -0400

    just removed coment

--- src/main/java/com/voiceit/web/PartyController.java
+++ src/main/java/com/voiceit/web/PartyController.java
@@ -22,10 +22,7 @@ public class PartyController {
 	
 	@Autowired
 	private UserRepository userRepository;
-	
-	@Autowired
-	private PartyReposiorty partyReposiorty;
-	
+		
 	@Autowired
 	private VoteReposiorty voteReposiorty;
 	
@@ -60,34 +57,14 @@ public class PartyController {
 	
 	@GetMapping("/edit/{id}")
 	public String editCurrentParty(ModelMap model, @PathVariable Long id) {
-//		Optional<Party> currParty = partyReposiorty.findById(id);
 		Party party = partyService.findPartyById(id);
 		model.put("party", party);
-//		model.put("currParty", currParty);
 		
 		return "editparty";
 	}
 	
 	@PostMapping("/edit/{id}")
 	public String saveEditedParty(Party party) {
-//		Optional<Party> currPartyOpt = partyReposiorty.findById(party.getId());
-////		 if(currPartyOpt.isPresent()) {
-////	        	currPartyOpt.get();
-////	        } else {
-////	        	throw new RuntimeException(" party not found for id >>>>>>> : " + party.getId());
-////	        }
-//		if(currPartyOpt.isPresent()) {
-//			Party nParty = currPartyOpt.get();
-//			nParty.setId(party.getId());
-//			nParty.setName(party.getName());
-//			
-//			partyReposiorty.save(nParty);
-////			 return nparty;
-//			
-//		} else {
-//			partyReposiorty.save(party);
-//			// return party;
-//		}
 		partyService.updateParty(party);
 		return "redirect:/";
 	}
